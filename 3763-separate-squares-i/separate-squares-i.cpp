e1eccf90830bd298cc92f1fdf47b37abb4e0ad58
Shreyag079 <155972795+Shreyag079@users.noreply.github.com>
2026-01-14 02:01:45 +0530

    Time: 195 ms (15.70%) | Memory: 198.6 MB (88.95%) - LeetSync

---
+++ 3763-separate-squares-i/separate-squares-i.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    double separateSquares(vector<vector<int>>& squares) {
+        double totalArea = 0.0;
+        double low = 1e9, high = -1e9;
+
+    
+        for (auto &sq : squares) 
+        {
+            double y = sq[1];
+            double l = sq[2];
+            totalArea += l * l;
+            low = min(low, y);
+            high = max(high, y + l);
+        }
+
+        double target = totalArea / 2.0;
+
+        
+        for (int i = 0; i < 100; i++) 
+        { 
+            double mid = (low + high) / 2.0;
+            double areaBelow = 0.0;
+
+            for (auto &sq : squares) 
+            {
+                double y = sq[1];
+                double l = sq[2];
+
+                if (mid <= y) 
+                {
+                    
+                    continue;
+                } 
+                else if (mid >= y + l) 
+                {
+                    
+                    areaBelow += l * l;
+                } 
+                else
+                {
+                    
+                    areaBelow += l * (mid - y);
+                }
+            }
+
+            if (areaBelow < target)
+                low = mid;
+            else
+                high = mid;
+        }
+
+        return (low + high) / 2.0;
+    }
+};
